00eed759489e5df03604c993d9890ca71a1ce391
Yago Lopes <yagolopeslazaro@gmail.com>
2020-07-19 18:12:43 -0300

    ad second par tab-navigation css

--- Youtube/Code-Drops/tab-navigation/script.js
+++ Youtube/Code-Drops/tab-navigation/script.js
@@ -10,30 +10,51 @@
 // lembrando que o bind serve para ligar o escopo de volta para o document
 
 const html = {
-  links: document.querySelector(".tab-links"),
-  contents: document.querySelector(".tab-content"),
+  links: [...document.querySelector(".tab-links").children],
+  contents: [...document.querySelector(".tab-content").children],
+  openTab: document.querySelector("[data-open]"),
 };
 
 //Todas as funções que estão dentro de tabNavigation, obviamente é do contexto tabNavigation
 
 function tabNavigation() {
   function hideAllTabContent() {
-    const contents = [...html.contents.children]; //Espalha os elementos em um array
+    // const contents = [...html.contents.children]; //Espalha os elementos em um array
 
-    contents.forEach((section) => {
+    html.contents.forEach((section) => {
       section.styles.display = "none";
     });
   }
 
-  function removeAllActiveClass() {}
+  function removeAllActiveClass() {
+    html.links.forEach((tab) => {
+      tab.className = tab.className.replace(" active", "");
+    });
+  }
 
-  function showCurrentTab() {}
+  function showCurrentTab(id) {
+    const tabContent = document.querySelector("#" + id);
+    tabContent.styles.display = "block";
+  }
 
-  function listenForChange() {}
+  function selectTab(event) {
+    hideAllTabContent();
+    removeAllActiveClass();
+    const target = event.currentTarget;
+    showCurrentTab(target.dataset.id);
+    target.className += " active";
+  }
+
+  function listenForChange() {
+    html.links.forEach((tab) => {
+      tab.addEventListener("click", selectTab);
+    });
+  }
 
   function init() {
     hideAllTabContent();
     listenForChange();
+    html.openTab.click();
   }
 
   return {
